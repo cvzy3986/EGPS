9dd97a8f3c7d947ffdd946026a2a172b58c4a09e
sinwook <mr_zealot@naver.com>
2016-11-26 14:48:04 +0900

    주석 처리

--- EGPS/src/TabChangeListener.java
+++ EGPS/src/TabChangeListener.java
@@ -5,7 +5,14 @@ import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-class TabChangeListener implements ChangeListener {
+/**
+ * @file TabChangeListener.java
+ * @brief 층 별 화면 출력 클래스
+ * @param arrFloor : 각 층의 이미지를 저장하고 있는 배열
+ * @remark 탭에 있는 각 층을 클릭 시 층에 따른 화면을 출력시켜준다. 
+ */
+
+public class TabChangeListener implements ChangeListener {
 	MapImage mapImage;
 	ArrayList<Image> arrFloor;
 	TabChangeListener(MapImage mapImage, ArrayList<Image> arrFloor){
